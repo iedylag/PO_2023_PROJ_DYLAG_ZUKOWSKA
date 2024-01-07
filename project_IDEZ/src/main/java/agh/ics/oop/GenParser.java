bc606780548f537bf8d79987560aa4f73e225bf0
ewazukowska <ewa795738@wp.pl>
2024-01-07 21:43:41 +0100

    a

--- project_IDEZ/src/main/java/agh/ics/oop/GenParser.java
+++ project_IDEZ/src/main/java/agh/ics/oop/GenParser.java
@@ -1,6 +1,6 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Rotation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,22 +10,22 @@ import java.util.List;
 
 
 public class GenParser {
-    public static List<MoveDirection> parse(String[] args) {
-        List<MoveDirection> directions = new ArrayList<>();
+    public static List<Rotation> parse(String[] args) {
+        List<Rotation> directions = new ArrayList<>();
 
         for (String arg : args) {
             if (arg == null) {
                 throw new IllegalArgumentException(arg + " is not legal move specification");
             }
-            MoveDirection direction = switch (arg) {
-                case "0" -> MoveDirection.FORWARD;
-                case "1" -> MoveDirection.BACKWARD;
-                case "2" -> MoveDirection.LEFT;
-                case "3" -> MoveDirection.RIGHT;
-                case "4" -> MoveDirection.FORWARD;
-                case "5" -> MoveDirection.BACKWARD;
-                case "6" -> MoveDirection.LEFT;
-                case "7" -> MoveDirection.RIGHT;
+            Rotation direction = switch (arg) {
+                case "0" -> Rotation.STRAIGHT;
+                case "1" -> Rotation.DEGREE45;
+                case "2" -> Rotation.DEGREE90;
+                case "3" -> Rotation.DEGREE135;
+                case "4" -> Rotation.DEGREE180;
+                case "5" -> Rotation.DEGREE225;
+                case "6" -> Rotation.DEGREE270;
+                case "7" -> Rotation.DEGREE315;
                 default -> throw new IllegalArgumentException(arg + " is not legal move specification"); //unchecked
             };
             directions.add(direction);
--- project_IDEZ/src/main/java/agh/ics/oop/World.java
+++ project_IDEZ/src/main/java/agh/ics/oop/World.java
@@ -1,7 +1,38 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.ConsoleMapDisplay;
+import agh.ics.oop.model.GrassField;
+import agh.ics.oop.model.Rotation;
+import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.WorldMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class World {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Start");
+
+        List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
+        ConsoleMapDisplay display = new ConsoleMapDisplay();
+
+        try {
+            List<Rotation> directions = GenParser.parse(args);
+            List<Simulation> simulations = new ArrayList<>();
+
+            for (int i = 0; i < 1; i++) {
+                WorldMap grassField = new GrassField(10, 10, 10); //to ma byc ustawiane przez użytkownika
+                grassField.subscribe(display);
+                simulations.add(new Simulation(directions, positions, grassField));
+            }
+
+            SimulationEngine engine = new SimulationEngine(simulations);
+            engine.runAsyncInThreadPool();
+            engine.awaitSimulationsEnd();
+        } catch (IllegalArgumentException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Stop");
     }
 }
