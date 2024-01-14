b7d2359986798380d741e932c87db6ee2a0a8b24
ewazukowska <ewa795738@wp.pl>
2024-01-14 13:50:14 +0100

    próby jak obsłużyć wariant zdechłe truchła

--- project_IDEZ/src/main/java/agh/ics/oop/model/DeadBodyFarm.java
+++ project_IDEZ/src/main/java/agh/ics/oop/model/DeadBodyFarm.java
@@ -1,5 +1,7 @@
 package agh.ics.oop.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -8,23 +10,28 @@ import java.util.List;
 // I CODZIENNIE PREFEROWANE POZYCJE DO GENEROWANIA ROSLIN BEDA SIE ZMIENIAC
 // JESLI UZYTKOWNIK USTAWI, ZE CODZIENNIE MA SIE DODAWAC 10 ROSLIN TO 8 Z NICH POWINNO SIE WYLOSOWAC Z POZYCJI GETNEARBODYPOSITIONS, A 2 Z RESZTY POZYCJI
 public class DeadBodyFarm extends WorldMap {
+    List<Animal> deadAnimalList = new ArrayList<>(deadAnimals.values());
 
     public DeadBodyFarm(int grassCount, int height, int width) {
         super(grassCount, height, width);
-        grassNearBodyGenerate(grassCount);
     }
-
+//mamy hash mape ze zwierzakami z ich pozycjami
     private List<Vector2d> getNearBodyPositions(List<Animal> deadAnimals) { //metoda zwracajaca pozycje wokol wszystkich martwych zwierzat
         return deadAnimals.stream()
                 .flatMap(deadAnimal -> deadAnimal.getPosition().around().stream())
                 .toList();
     }
 
-    private void grassNearBodyGenerate(int newGrassCount) { //losowanie okreslonej liczby pozycji z getNearBodyPositions
-        if (Math.random() < 0.8) {
-            //wylosuj pozycje wokol truchla  (w sumie ma byc zawsze 80% wszystkich roslin)
-        } else {
-            //wylosuj pozycje spoza rownika (w sumie ma byc zawsze 20% wszystkich roslin)
+    @Override
+    public void generateFromPreferablePosition() {
+        int preferableGrassPlaces = (int) (0.2 * width * height);
+        List<Vector2d> possiblePositions = getNearBodyPositions(deadAnimalList);
+        if (preferableGrassPlaces < possiblePositions.size()){
+            Collections.shuffle(possiblePositions);
+            possiblePositions = possiblePositions.subList(0, 1);
+            for (Vector2d grassPosition : possiblePositions) {
+                grasses.put(grassPosition, new Grass(grassPosition));
+            }
         }
     }
 }
